a1a70137f0551a847cc8aad06e95b3a2654d368f
soxfan1966 <49078444+soxfan1966@users.noreply.github.com>
2021-10-01 14:26:00 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 install.packages(c("base",
                    "datasets",
-                   "methids",
+                   "methods",
                    "stats",
                    "utils",
                    "graphics",
